6ede46045eb5925a26e15210ac385f7fc84c3171
wiktor <wiktor.kycia@gmail.com>
2025-12-03 15:07:58 +0100

    remove the allocate method from Filter class

--- filter.cpp
+++ filter.cpp
@@ -4,38 +4,55 @@
 Filter::Filter()
 {
     size = 1;
-    factors = nullptr;
+    this->factors = new int8_t*[size];
+    for(unsigned int i = 0; i < size; i++)
+    {
+        this->factors[i] = new int8_t[size];
+        for(unsigned int j = 0; j < size; j++)
+        {
+            this->factors[i][j] = 0;
+        }
+    }
+    this->set_factor(0,0,1);
 }
 
 Filter::Filter(unsigned int _size)
 {
     this->size = _size;
+
+    unsigned int arraySize = 2*(this->size)+1;
+    this->factors = new int8_t*[arraySize];
+    for(unsigned int i = 0; i < arraySize; i++)
+    {
+        this->factors[i] = new int8_t[arraySize];
+        for(unsigned int j = 0; j < arraySize; j++)
+        {
+            this->factors[i][j] = 0;
+        }
+    }
+    this->set_factor(0,0,1);
 }
 
-Filter::Filter(int8_t **factors)
+Filter::Filter(unsigned int _size, int8_t **_factors)
 {
-    this->factors = factors;
+    this->size = _size;
+    unsigned int arraySize = 2*(this->size)+1;
+
+    this->factors = new int8_t*[arraySize];
+    for(unsigned int i = 0; i < arraySize; i++)
+    {
+        this->factors[i] = new int8_t[arraySize];
+        for(unsigned int j = 0; j < arraySize; j++)
+        {
+            this->factors[i][j] = _factors[i][j];
+        }
+    }
 }
 
 Filter::~Filter()
 {
     size = 0;
     delete factors;
-}
-
-void Filter::allocate()
-{
-    unsigned int _size = 2*(this->size)+1;
-    this->factors = new int8_t*[_size];
-    for(unsigned int i = 0; i < _size; i++)
-    {
-        this->factors[i] = new int8_t[_size];
-        for(unsigned int j = 0; j < _size; j++)
-        {
-            this->factors[i][j] = 0;
-        }
-    }
-    this->set_factor(0,0,1);
 }
 
 
@@ -47,11 +64,16 @@ int8_t Filter::get_factor(int posx, int posy)
     return factors[y][x];
 }
 
-void Filter::set_factor(int posx, int posy, int8_t new_value)
+void Filter::set_factor(int posx, int posy, const int8_t &new_value)
 {
     int x = posx + this->size;
     int y = posy + this->size;
 
-    factors[y][x] = new_value;
+    unsigned int arraySize = 2 * size + 1;
+
+    if (x < 0 || y < 0 || x >= arraySize || y >= arraySize)
+        return;
+    else
+        this->factors[y][x] = new_value;
 }
 
--- filter.h
+++ filter.h
@@ -5,15 +5,14 @@
 class Filter
 {
     int8_t **factors;
-    void allocate();
 public:
     unsigned int size;
     int8_t get_factor(int posx, int posy);
-    void set_factor(int posx, int posy, int8_t new_value);
+    void set_factor(int posx, int posy, const int8_t &new_value);
 
     Filter();
     Filter(unsigned int _size);
-    Filter(int8_t **_factors);
+    Filter(unsigned int _size, int8_t **_factors);
     ~Filter();
 };
 
